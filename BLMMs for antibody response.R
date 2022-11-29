4d49c9c54948c6f66ce17ad2d798a1b5f9d33e0a
Jia Wei <jiawei@NDMs-MacBook-Pro.local>
2022-11-29 16:36:37 +0000

    Initial commit

---
+++ BLMMs for antibody response.R
@@ -0,0 +1,183 @@
+####################################################################################################
+# Example of code written by Jia Wei (Nuffield Department of Medicine, University of Oxford)       #
+# for analyses of antibody waning following booster vaccination or breakthrough infection.         #
+# Accompanying paper/preprint: Correlates of protection against SARS-CoV-2 Omicron variant and     #
+# anti-spike antibody responses after a third/booster vaccination or breakthrough infection in the #
+# UK general population                                                                            #
+####################################################################################################
+
+library(tidyverse)
+library(brms)
+
+
+###piecewise model examining antibody responses from 21 days post-second vaccination to 14 days post-third/booster vaccination or infection 
+
+data1=data1 %>% 
+  filter(time<=14) %>% 
+  mutate(knot1=0) %>% 
+  mutate(T1=(time<knot1)*time7+(time>=knot1)*knot1,
+         T2=(time>=knot1)*(time-knot1)+(time<knot1)*knot1) %>% 
+  group_by(id)%>%
+  mutate(ID=cur_group_id()) %>% 
+  ungroup() %>% 
+  mutate(ID=as.factor(ID))
+
+priors=c(
+  set_prior("normal(6,2)",class="Intercept"),
+  set_prior("normal(0,1)",coef="T1",class="b"),
+  set_prior("normal(0,1)",coef="T2",class="b"),
+  set_prior("normal(0,1)",coef="age10",class="b"),
+  set_prior("normal(0,1)",coef="Male1",class="b"),
+  set_prior("normal(0,1)",coef="ethnicity1",class="b"),
+  set_prior("normal(0,1)",coef="lthc1",class="b"),
+  set_prior("normal(0,1)",coef="hcw1",class="b"),
+  set_prior("normal(0,1)",coef="dur30",class="b"),
+  
+  set_prior("normal(0,0.1)",coef="age10:T1",class="b"),
+  set_prior("normal(0,0.1)",coef="T2:age1",class="b"),
+  set_prior("normal(0,0.1)",coef="T2:age2",class="b"),
+  
+  set_prior("normal(0,0.1)",coef="T1:Male1",class="b"),
+  set_prior("normal(0,0.1)",coef="T2:Male1",class="b"),
+  
+  set_prior("normal(0,0.1)",coef="T1:ethnicity1",class="b"),
+  set_prior("normal(0,0.1)",coef="T2:ethnicity1",class="b"),
+  
+  set_prior("normal(0,0.1)",coef="T1:lthc1",class="b"),
+  set_prior("normal(0,0.1)",coef="T2:lthc1",class="b"),
+  
+  set_prior("normal(0,0.1)",coef="T1:hcw1",class="b"),
+  set_prior("normal(0,0.1)",coef="T2:hcw1",class="b"),
+  
+  set_prior("normal(0,0.1)",coef="T1:dur30",class="b"),
+  set_prior("normal(0,0.1)",coef="dur30:T2",class="b"),
+  
+  set_prior("cauchy(0,1)",coef="T1",class="sd",group="ID"),
+  set_prior("cauchy(0,1)",coef="T2",class="sd",group="ID"),
+  set_prior("cauchy(0,1)",coef="Intercept",class="sd",group="ID"),
+  set_prior("cauchy(0,1)",class="sigma"),
+  set_prior("lkj_corr_cholesky(1)",class="L")
+)
+
+
+set_inits=function(seed=1){
+  set.seed(seed)
+  list(
+    Intercept=rnorm(1,6,2),
+    b=c(rnorm(1,0,1),#T1
+        rnorm(1,0,1),#T2
+        rnorm(1,0,1),#age
+        rnorm(1,0,1),#sex
+        rnorm(1,0,1),#ethnicity
+        rnorm(1,0,1),#lthc
+        rnorm(1,0,1),#hcw
+        rnorm(1,0,1),#dur
+        
+        rnorm(1,0,1),#age:T1
+        rnorm(1,0,1),#age:T2
+        rnorm(1,0,1),#age:T2
+        rnorm(1,0,1),#sex:T1
+        rnorm(1,0,1),#sex:T2
+        rnorm(1,0,1),#ethnicity:T1
+        rnorm(1,0,1),#ethnicity:T2
+        rnorm(1,0,1),#lthc:T1
+        rnorm(1,0,1),#lthc:T2
+        rnorm(1,0,1),#hcw:T1
+        rnorm(1,0,1),#hcw:T2
+        rnorm(1,0,1),#dur:T1
+        rnorm(1,0,1) #prior:T2
+    ),
+    sigma=runif(1,0,1),
+    sd_1=c(runif(1,0,1),runif(1,0,1),runif(1,0,1)),
+    z_1=matrix(rep(c(6,-0.1,1),21448),3,21448)
+  )
+}
+
+inits_list=list(
+  set_inits(1),
+  set_inits(2),
+  set_inits(3),
+  set_inits(4),
+  set_inits(5),
+  set_inits(6)
+)
+
+fit1 <- brm(formula=log2(assay)|cens(cen1)~1+T1:age10+age10+T2:age1+T2:age2+
+                       T1*dur30+T2*dur30+T1*Male+T2*Male+
+                       T1*ethnicity+T2*ethnicity+
+                       T1*lthc+T2*lthc+T1*hcw+T2*hcw+(1+T1+T2|ID),
+                     data=data1,cores=6,family=gaussian(),
+                     prior = priors,inits=inits_list,
+                     chains = 6, iter=4000, warmup = 2000, seed=42,
+                     control = list(adapt_delta=0.95))
+
+
+## linear model examining antibody decline from 42 days post third/booster vaccination or infection
+
+priors=c(
+  set_prior("normal(10,2)",class="Intercept"),
+  set_prior("normal(0,0.5)",coef="T3",class="b"),
+  set_prior("normal(0,1)",coef="age1",class="b"),
+  set_prior("normal(0,1)",coef="age2",class="b"),
+  set_prior("normal(0,1)",coef="Male1",class="b"),
+  set_prior("normal(0,1)",coef="ethnicity1",class="b"),
+  set_prior("normal(0,1)",coef="lthc1",class="b"),
+  set_prior("normal(0,1)",coef="hcw1",class="b"),
+  set_prior("normal(0,1)",coef="dur30",class="b"),
+  
+  set_prior("normal(0,0.1)",coef="T3:age1",class="b"),
+  set_prior("normal(0,0.1)",coef="T3:age2",class="b"),
+  set_prior("normal(0,0.1)",coef="T3:Male1",class="b"),
+  set_prior("normal(0,0.1)",coef="T3:ethnicity1",class="b"),
+  set_prior("normal(0,0.1)",coef="T3:lthc1",class="b"),
+  set_prior("normal(0,0.1)",coef="T3:hcw1",class="b"),
+  set_prior("normal(0,0.1)",coef="T3:dur30",class="b"),
+  
+  set_prior("normal(0,0.1)",coef="T3",class="sd",group="ID"),
+  set_prior("normal(0,1)",coef="Intercept",class="sd",group="ID"),
+  set_prior("normal(0,1)",class="sigma"),
+  set_prior("lkj_corr_cholesky(1)",class="L")
+)
+
+set_inits=function(seed=1){
+  set.seed(seed)
+  list(
+    Intercept=rnorm(1,10,2),
+    b=c(rnorm(1,0,0.5),#time
+        rnorm(1,0,1),#age1
+        rnorm(1,0,1),#age2
+        rnorm(1,0,1),#sex
+        rnorm(1,0,1),#ethnicity
+        rnorm(1,0,1),#lthc
+        rnorm(1,0,1),#hcw
+        rnorm(1,0,1),#dur
+        
+        rnorm(1,0,0.1),#age1
+        rnorm(1,0,0.1),#age2
+        rnorm(1,0,0.1),#sex
+        rnorm(1,0,0.1),#ethnicity
+        rnorm(1,0,0.1),#lthc
+        rnorm(1,0,0.1),#hcw
+        rnorm(1,0,0.1),#dur
+    ),
+    sigma=runif(1,0,1),
+    sd_1=c(runif(1,0,1),runif(1,0,0.1)),
+    z_1=matrix(rep(c(10,-0.1),14748),2,14748)
+  )
+}
+
+inits_list=list(
+  set_inits(1),
+  set_inits(2),
+  set_inits(3),
+  set_inits(4),
+  set_inits(5),
+  set_inits(6)
+)
+
+fit2 <- brm(formula=log2(assay)|cens(cen1)~1+T3*age1+T3*age2+T3*Male+
+              T3*ethnicity+T3*lthc+T3*hcw+T3*dur30+(1+T3|ID),
+                          data=data2,cores=6,family=gaussian(),
+                          prior = priors,inits=inits_list,
+                          chains = 6, iter=4000, warmup = 2000, seed=42,
+                          control = list(adapt_delta=0.95))
---
+++ GAMs for correlates of protection.R
@@ -0,0 +1,187 @@
+####################################################################################################
+# Example of code written by Jia Wei (Nuffield Department of Medicine, University of Oxford)       #
+# for analyses of correlates of protection against SARS-Cov-2 infection.                           #
+# Accompanying paper/preprint: Correlates of protection against SARS-CoV-2 Omicron variant and     #
+# anti-spike antibody responses after a third/booster vaccination or breakthrough infection in the #
+# UK general population                                                                            #
+####################################################################################################
+
+library(mgcv)
+library(dplyr)
+
+## model fitting
+m <- bam(result_mk ~ s(assay_21_59,k=200)+s(dur_vac_inf,k=20)+
+           s(age_at_visit,k=20)+s(study_day,k=20)+
+           ti(assay_21_59,dur_vac_inf,k=c(200,20)) +
+           ti(assay_21_59,age_at_visit,k=c(200,20)) + variant+
+           ti(study_day, age_at_visit, k=c(20,20), by=region_model) + 
+           ethnicity_wo + sex + region_model + 
+           imd_pc_new + multigen + hhsizegroup + rural_urban_class +
+           ever_care_home_worker + patient_facing_clean_ever + 
+           ever_personfacing_socialcare + ever_lthc + health_condition+
+           visit_freq + smoke_now + contact_hospital + contact_carehome, data = data, 
+         family = binomial,method="fREML",discrete=TRUE,nthreads=8)
+
+
+##function to extract odds ratio
+getor_vaccine_type <- function(newdata = newdata, contrast = c(2,8), model=model) {
+  newdata$assay_21_59 <- c(contrast[1],contrast[2])
+  Xp <- predict(model, newdata = newdata, type="lpmatrix")
+  diff <- (Xp[2,]-Xp[1,])
+  dly <- t(diff)%*%coef(model)
+  se.dly <- sqrt(t(diff)%*%vcov(model)%*%diff)
+  c(exp(dly), exp(dly -2*se.dly), exp(dly + 2*se.dly))
+}
+
+
+
+
+## prediction for vaccination group
+
+age_list=seq(20,80,5)
+results_list=list()
+j=1
+for(age in age_list){
+  newdata = data.frame(age_at_visit=age, sex=2,ethnicity_wo=1,study_day=400, region_model=8,imd_pc_new=60,multigen=1,
+                       hhsizegroup=2,rural_urban_class=2, ever_care_home_worker=1,patient_facing_clean_ever=1, ever_personfacing_socialcare=1,
+                       ever_lthc=1,visit_freq=1,smoke_now=1,contact_hospital=1,contact_carehome=1,dur_vac_inf=210,variant="Vaccination",health_condition="No")
+  newdata <- rbind(newdata,newdata)
+  
+  dat_ors_vac <- data.frame(assay_round = seq(0,8000,20))
+  dat_ors_vac$or <- NA
+  dat_ors_vac$ll <- NA
+  dat_ors_vac$ul <- NA
+  
+  
+  for (i in 1:nrow(dat_ors_vac)) {
+    temp <- getor_vaccine_type(newdata=newdata,  contrast=c(0,dat_ors_vac$assay_round[i]),
+                               model = m)
+    dat_ors_vac$or[i] <- temp[1]
+    dat_ors_vac$ll[i] <- temp[2]
+    dat_ors_vac$ul[i] <- temp[3]
+  }
+  
+  dat_ors_vac=dat_ors_vac%>%mutate(VE=100*(1-or),VE_ll=100*(1-ul),VE_ul=100*(1-ll))
+  dat_ors_vac$variant="Vaccination"
+  dat_ors_vac$age=age
+  
+  results_list[[j]]<-dat_ors_vac
+  j=j+1
+}
+
+results_vac=do.call(rbind,results_list)
+
+
+
+## prediction for Pre-Alpha/Alpha group
+
+age_list=seq(20,80,5)
+results_list=list()
+j=1
+for(age in age_list){
+  newdata = data.frame(age_at_visit=age, sex=2,ethnicity_wo=1,study_day=400, region_model=8,imd_pc_new=60,multigen=1,
+                       hhsizegroup=2,rural_urban_class=2, ever_care_home_worker=1,patient_facing_clean_ever=1, ever_personfacing_socialcare=1,
+                       ever_lthc=1,visit_freq=1,smoke_now=1,contact_hospital=1,contact_carehome=1,dur_vac_inf=210,health_condition="No")
+  newdata <- rbind(newdata,newdata)
+  newdata$variant <- c("Vaccination","Pre-Alpha/Alpha")
+  
+  dat_ors_vac <- data.frame(assay_round = seq(0,8000,20))
+  dat_ors_vac$or <- NA
+  dat_ors_vac$ll <- NA
+  dat_ors_vac$ul <- NA
+  
+  
+  for (i in 1:nrow(dat_ors_vac)) {
+    temp <- getor_vaccine_type(newdata=newdata,  contrast=c(0,dat_ors_vac$assay_round[i]),
+                               model = m)
+    dat_ors_vac$or[i] <- temp[1]
+    dat_ors_vac$ll[i] <- temp[2]
+    dat_ors_vac$ul[i] <- temp[3]
+  }
+  
+  dat_ors_vac=dat_ors_vac%>%mutate(VE=100*(1-or),VE_ll=100*(1-ul),VE_ul=100*(1-ll))
+  dat_ors_vac$variant="Pre-alpha/Alpha"
+  dat_ors_vac$age=age
+  
+  results_list[[j]]<-dat_ors_vac
+  j=j+1
+}
+
+results_alpha=do.call(rbind,results_list)
+
+
+
+
+## prediction for Delta/BA.1 group
+
+age_list=seq(20,80,5)
+results_list=list()
+j=1
+for(age in age_list){
+  newdata = data.frame(age_at_visit=age, sex=2,ethnicity_wo=1,study_day=400, region_model=8,imd_pc_new=60,multigen=1,
+                       hhsizegroup=2,rural_urban_class=2, ever_care_home_worker=1,patient_facing_clean_ever=1, ever_personfacing_socialcare=1,
+                       ever_lthc=1,visit_freq=1,smoke_now=1,contact_hospital=1,contact_carehome=1,dur_vac_inf=180,health_condition="No")
+  newdata <- rbind(newdata,newdata)
+  newdata$variant <- c("Vaccination","Delta/BA.1")
+  
+  dat_ors_vac <- data.frame(assay_round = seq(0,8000,20))
+  dat_ors_vac$or <- NA
+  dat_ors_vac$ll <- NA
+  dat_ors_vac$ul <- NA
+  
+  
+  for (i in 1:nrow(dat_ors_vac)) {
+    temp <- getor_vaccine_type(newdata=newdata,  contrast=c(0,dat_ors_vac$assay_round[i]),
+                               model = m)
+    dat_ors_vac$or[i] <- temp[1]
+    dat_ors_vac$ll[i] <- temp[2]
+    dat_ors_vac$ul[i] <- temp[3]
+  }
+  
+  dat_ors_vac=dat_ors_vac%>%mutate(VE=100*(1-or),VE_ll=100*(1-ul),VE_ul=100*(1-ll))
+  dat_ors_vac$variant="Delta/BA.1"
+  dat_ors_vac$age=age
+  
+  results_list[[j]]<-dat_ors_vac
+  j=j+1
+}
+
+results_delta=do.call(rbind,results_list)
+
+
+## prediction for BA.2 group
+
+age_list=seq(20,80,5)
+results_list=list()
+j=1
+for(age in age_list){
+  newdata = data.frame(age_at_visit=age, sex=2,ethnicity_wo=1,study_day=400, region_model=8,imd_pc_new=60,multigen=1,
+                       hhsizegroup=2,rural_urban_class=2, ever_care_home_worker=1,patient_facing_clean_ever=1, ever_personfacing_socialcare=1,
+                       ever_lthc=1,visit_freq=1,smoke_now=1,contact_hospital=1,contact_carehome=1,dur_vac_inf=120,health_condition="No")
+  newdata <- rbind(newdata,newdata)
+  newdata$variant <- c("Vaccination","BA.2")
+  
+  dat_ors_vac <- data.frame(assay_round = seq(0,8000,20))
+  dat_ors_vac$or <- NA
+  dat_ors_vac$ll <- NA
+  dat_ors_vac$ul <- NA
+  
+  
+  for (i in 1:nrow(dat_ors_vac)) {
+    temp <- getor_vaccine_type(newdata=newdata,  contrast=c(0,dat_ors_vac$assay_round[i]),
+                               model = m)
+    dat_ors_vac$or[i] <- temp[1]
+    dat_ors_vac$ll[i] <- temp[2]
+    dat_ors_vac$ul[i] <- temp[3]
+  }
+  
+  dat_ors_vac=dat_ors_vac%>%mutate(VE=100*(1-or),VE_ll=100*(1-ul),VE_ul=100*(1-ll))
+  dat_ors_vac$variant="BA.2"
+  dat_ors_vac$age=age
+  
+  results_list[[j]]<-dat_ors_vac
+  j=j+1
+}
+
+results_ba2=do.call(rbind,results_list)
+
